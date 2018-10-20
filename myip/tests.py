6a5c8b329a5c2703d25381466da3d32c87fda327
Junior <junior@iforge.com.br>
2018-10-20 11:54:09 +0200

    Language files fix and tests

--- myip/tests.py
+++ myip/tests.py
@@ -12,7 +12,6 @@ class IndexViewTests(SimpleTestCase):
         self.assertTrue('no-store' in response['Cache-Control'])
         self.assertTrue('must-revalidate' in response['Cache-Control'])
 
-
     def test_display_remote_ip_in_title(self):
         remote_ip = '10.10.10.10'
         response = self.client.get(reverse('myip:index'), REMOTE_ADDR=remote_ip)
@@ -37,5 +36,23 @@ class IndexViewTests(SimpleTestCase):
     def test_get_hostname(self):
         self.assertEqual(myip.views.get_hostname('127.0.0.1'), 'localhost')
         self.assertEqual(myip.views.get_hostname('203.0.113.0'), '') # TEST_NET-3
-
-
+    
+    def test_language_support_english(self):
+        response = self.client.get(reverse('myip:index'), HTTP_ACCEPT_LANGUAGE='en')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'My IP address is')
+    
+    def test_language_support_german(self):
+        response = self.client.get(reverse('myip:index'), HTTP_ACCEPT_LANGUAGE='de')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Meine IP-Adresse ist')
+    
+    def test_language_support_spanish(self):
+        response = self.client.get(reverse('myip:index'), HTTP_ACCEPT_LANGUAGE='es')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Mi dirección IP es')
+    
+    def test_language_support_portuguese(self):
+        response = self.client.get(reverse('myip:index'), HTTP_ACCEPT_LANGUAGE='pt-BR')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Meu endereço IP é')
